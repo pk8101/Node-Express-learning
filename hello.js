08a37736ead7157acb3b5936b6efd11db91b19e9
pk8101 <pradeepvirat913@gmail.com>
2023-03-02 01:15:17 +0530

    assert.equal()

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const assert=require('assert').strict;
 let x=5,y=4;
 try{
-    assert.deepStrictEqual(x===y,x!==y)//checking condition
+    assert.equal(x,y)//checking condition
 }
 catch(error){
     console.log('error',error)
