203cdfe165f2f5c7f58008b6dc538a82f861bca6
pk8101 <pradeepvirat913@gmail.com>
2023-03-02 01:19:37 +0530

    assert.ifError()

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const assert=require('assert').strict;
 let x=5,y=4;
 try{
-    assert.equal(x,y)//checking condition
+    assert.ifError(undefined)//this ifError only accept null and undefined and every thing will be error;
 }
 catch(error){
     console.log('error',error)
