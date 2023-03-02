36d414b722b7292b3ae5d369563eb5a7d0338b96
pk8101 <pradeepvirat913@gmail.com>
2023-03-02 23:49:56 +0530

    request

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http =require('http');
 port =5000;
 http.createServer((req,res)=>{
-  res.write('hello World');
+  res.write('hello World ');
   res.end('example for server');
-}).listen(port);
+}).listen(port,()=>{
+  console.log('server started !!')
+});
 
+var options={
+  method:'GET',
+  path:'/courses',
+  host:'www.geeksforgeeks.org',
+}
+http.request(options,(res)=>{
+  console.log(`StatusCode:${res.statusCode}`)
+}).end()
